2b2e4b36cd560ac65c6440366ae70c87b6d5fd2c
User <user@example.com>
2026-01-23 16:44:08 +0300

    fix: enable Google Pay/Apple Pay funding sources

--- src/components/PayPalPayment.tsx
+++ src/components/PayPalPayment.tsx
@@ -10,7 +10,8 @@ export default function PayPalPayment() {
         clientId: "AQNoA7KjWTwuNVuhy9nKnBzc9jHp6mFif6vFOgLkm7N2M5aHHXHTicVNr09mg_9hJemDso1H2UXwDeDA",
         currency: "USD",
         intent: "capture",
-        disableFunding: "applepay,venmo",
+        components: "buttons",
+        "enable-funding": "googlepay,applepay",
     };
 
     return (
